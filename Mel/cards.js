ecef16247fd6cd0073f22f7de98d726c07947fd4
WebMasterIT <wspc777@yandex.ru>
2019-11-03 20:50:04 +0300

    Create Sort

---
+++ Mel/cards.js
@@ -0,0 +1,403 @@
+var data = [{
+        imgSrc: 'img/articles/10.png',
+        blogText: '"Мама почти ничего мне не запрещала": чего ждут от нас дети',
+        iconViews: '1 813',
+        iconComment: '19',
+        readTimeIcon: ` `,
+    },
+    {
+        imgSrc: 'img/articles/11.png',
+        blogText: 'Как мы учились этим летом: дни самоуправления, неделя Испании и уроки',
+        iconViews: ' 1 813',
+        iconComment: '21',
+        readTimeIcon: ` `,
+    },
+    {
+        imgSrc: 'img/articles/12.jpg',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 813',
+        iconComment: '22',
+        readTimeIcon: ` `,
+
+    },
+    {
+        imgSrc: 'img/articles/13.jpg',
+        blogText: 'Подростковые конфликты: как их решить личным примером',
+        iconViews: ' 1 899',
+        iconComment: '22',
+        readTimeIcon: ` `,
+    },
+    {
+        imgSrc: 'img/articles/14.jpg',
+        blogText: '"Мама почти ничего мне не запрещала": чего ждут от нас дети',
+        iconViews: '1 823',
+        iconComment: '18',
+        readTimeIcon: ` `,
+    },
+    {
+        imgSrc: 'img/articles/15.jpg',
+        blogText: 'Как мы учились этим летом: дни самоуправления, неделя Испании и уроки',
+        iconViews: ' 1 843',
+        iconComment: '24',
+    },
+    {
+        imgSrc: 'img/articles/16.jpg',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 813',
+        iconComment: '22',
+
+    },
+    {
+        imgSrc: 'img/articles/18.jpg',
+        blogText: 'Подростковые конфликты: как их решить личным примером',
+        iconViews: ' 1 849',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/19.jpg',
+        blogText: '"Мама почти ничего мне не запрещала": чего ждут от нас дети',
+        iconViews: '1 853',
+        iconComment: '19',
+    },
+    {
+        imgSrc: 'img/articles/20.jpg',
+        blogText: 'Как мы учились этим летом: дни самоуправления, неделя Испании и уроки',
+        iconViews: ' 1 822',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/21.jpg',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 811',
+        iconComment: '22',
+
+    },
+    {
+        imgSrc: 'img/articles/22.png',
+        blogText: 'Подростковые конфликты: как их решить личным примером',
+        iconViews: ' 1 809',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/23.jpg',
+        blogText: '"Мама почти ничего мне не запрещала": чего ждут от нас дети',
+        iconViews: '1 812',
+        iconComment: '19',
+        readTimeIcon: ` `,
+    },
+    {
+        imgSrc: 'img/articles/24.png',
+        blogText: 'Как мы учились этим летом: дни самоуправления, неделя Испании и уроки',
+        iconViews: ' 1 813',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/25.jpg',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 813',
+        iconComment: '22',
+
+    },
+    {
+        imgSrc: 'img/articles/26.jpg',
+        blogText: 'Подростковые конфликты: как их решить личным примером',
+        iconViews: ' 1 899',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/27.jpg',
+        blogText: '"Мама почти ничего мне не запрещала": чего ждут от нас дети',
+        iconViews: '1 813',
+        iconComment: '19',
+    },
+    {
+        imgSrc: 'img/articles/28.jpg',
+        blogText: 'Как мы учились этим летом: дни самоуправления, неделя Испании и уроки',
+        iconViews: ' 1 813',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/29.jpg',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 813',
+        iconComment: '22',
+
+    },
+    {
+        imgSrc: 'img/articles/30.jpg',
+        blogText: 'Подростковые конфликты: как их решить личным примером',
+        iconViews: ' 1 899',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/31.jpg',
+        blogText: '"Мама почти ничего мне не запрещала": чего ждут от нас дети',
+        iconViews: '1 813',
+        iconComment: '19',
+    },
+    {
+        imgSrc: 'img/articles/32.jpg',
+        blogText: 'Как мы учились этим летом: дни самоуправления, неделя Испании и уроки',
+        iconViews: ' 1 813',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/33.jpg',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 813',
+        iconComment: '22',
+        readTimeIcon: ` `,
+
+    },
+    {
+        imgSrc: 'img/articles/34.jpg',
+        blogText: 'Подростковые конфликты: как их решить личным примером',
+        iconViews: ' 1 899',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/35.jpg',
+        blogText: '"Мама почти ничего мне не запрещала": чего ждут от нас дети',
+        iconViews: '1 813',
+        iconComment: '19',
+    },
+    {
+        imgSrc: 'img/articles/36.jpg',
+        blogText: 'Как мы учились этим летом: дни самоуправления, неделя Испании и уроки',
+        iconViews: ' 1 813',
+        iconComment: '22',
+        readTimeIcon: ` `,
+    },
+    {
+        imgSrc: 'img/articles/37.png',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 813',
+        iconComment: '22',
+
+    },
+    {
+        imgSrc: 'img/articles/38.png',
+        blogText: 'Подростковые конфликты: как их решить личным примером',
+        iconViews: ' 1 899',
+        iconComment: '22',
+        readTimeIcon: ` `,
+    },
+    {
+        imgSrc: 'img/articles/39.jpg',
+        blogText: '"Мама почти ничего мне не запрещала": чего ждут от нас дети',
+        iconViews: '1 813',
+        iconComment: '19',
+    },
+    {
+        imgSrc: 'img/articles/40.jpg',
+        blogText: 'Как мы учились этим летом: дни самоуправления, неделя Испании и уроки',
+        iconViews: ' 1 813',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/41.jpg',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 813',
+        iconComment: '22',
+        readTimeIcon: ` `,
+    },
+    {
+        imgSrc: 'img/articles/42.png',
+        blogText: 'Подростковые конфликты: как их решить личным примером',
+        iconViews: ' 1 899',
+        iconComment: '22',
+        readTimeIcon: ` `,
+    },
+    {
+        imgSrc: 'img/articles/43.jpg',
+        blogText: '"Мама почти ничего мне не запрещала": чего ждут от нас дети',
+        iconViews: '1 813',
+        iconComment: '19',
+    },
+    {
+        imgSrc: 'img/articles/44.jpg',
+        blogText: 'Как мы учились этим летом: дни самоуправления, неделя Испании и уроки',
+        iconViews: ' 1 813',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/45.jpg',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 813',
+        iconComment: '22',
+
+    },
+    {
+        imgSrc: 'img/articles/46.jpg',
+        blogText: 'Подростковые конфликты: как их решить личным примером',
+        iconViews: ' 1 899',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/47.jpg',
+        blogText: '"Мама почти ничего мне не запрещала": чего ждут от нас дети',
+        iconViews: '1 813',
+        iconComment: '19',
+    },
+    {
+        imgSrc: 'img/articles/48.jpg',
+        blogText: 'Как мы учились этим летом: дни самоуправления, неделя Испании и уроки',
+        iconViews: ' 1 813',
+        iconComment: '22',
+    },
+    {
+        imgSrc: 'img/articles/49.jpg',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 813',
+        iconComment: '22',
+
+    },
+    {
+        imgSrc: 'img/articles/50.jpg',
+        blogText: '7 признаков классного учителя',
+        iconViews: ' 1 813',
+        iconComment: '22',
+        readTimeIcon: ` `,
+    }
+]
+
+
+
+// let articles = document.querySelector('#articles');
+// replacedNode = articles.replaceChild(articles.children[1], articles.children[0]);
+// console.log(replacedNode);
+// articles.appendChild(replacedNode);
+
+document.querySelector('#sort-asc').onclick = mySort;
+document.querySelector('#sort-desc').onclick = mySortDesc;
+
+function mySort() {
+    let articles = document.querySelector('#articles');
+    for (let i=0; i < articles.children.length; i++) {
+        for (let j=i; j < articles.children.length; j++) {
+            if (+articles.children[i].getAttribute('data-sort') > +articles.children[j].getAttribute('data-sort')) {
+                replacedNode = articles.replaceChild(articles.children[j], articles.children[i]); 
+                insertAfter(replacedNode, articles.children[i])
+            }
+        }
+    }
+}
+
+function mySortDesc() {
+    let articles = document.querySelector('#articles');
+    for (let i=0; i < articles.children.length; i++) {
+        for (let j=i; j < articles.children.length; j++) {
+            if (+articles.children[i].getAttribute('data-sort') < +articles.children[j].getAttribute('data-sort')) {
+                replacedNode = articles.replaceChild(articles.children[j], articles.children[i]); 
+                insertAfter(replacedNode, articles.children[i])
+            }
+        }
+    }
+}
+
+
+
+
+function insertAfter(elem, refElem) {
+    return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
+}
+
+
+function createCard(i) {
+
+    var card = document.createElement('article');
+    var imgSrc = data[i].imgSrc;
+    var text = data[i].blogText;
+    var iconViews = data[i].iconViews;
+    var iconComment = data[i].iconComment;
+
+    var description = getCardsInfo(data[i])
+    var wrapper = document.getElementById('articles');
+    card.innerHTML = `
+    <img src="${imgSrc}" alt="Изображение" title="Изображение">
+    <p class="cardsBlogs"> Блоги </p>
+    <p class="cardsText"> ${text}
+       <div class="articleInfo">
+         <div class="articleViwes">
+         ${description}
+       </div>
+    </div>`;
+    wrapper.appendChild(card);
+}
+
+function getCardsInfo(item) {
+    if (item.readTimeIcon) {
+        return ` <i class="fas fa-clock"></i> Читать: 3 минуты${item.readTimeIcon}`
+    } else {
+        return `<div class="articleViwes">
+      <i class="far fa-eye"></i> ${item.iconViews}
+      </div>
+      <div class="articleViwes">
+       <i class="fas fa-comment-alt"></i> ${item.iconComment}
+      </div>`;
+    }
+}
+
+function pastTemplate() {
+    var card = document.querySelector('#tmpl')
+    var imgSrc = data[i].imgSrc;
+    var text = data[i].blogText;
+    var iconViews = data[i].iconViews;
+    var iconComment = data[i].iconComment;
+
+    var cardContent = card.content;
+    cardContent.querySelector('.cardsText').innerText = `${text}`;
+    cardContent.querySelector(".articleInfo").innerHTML = `<i class="far fa-eye"></i> ${iconViews}
+    </div>
+    <div class="articleViwes">
+     <i class="fas fa-comment-alt"></i> ${iconComment}`;
+
+    cardContent.querySelector('.canvas').src = `${imgSrc}`;
+
+    var clone = document.importNode(card.content, true);
+
+    articles.appendChild(clone);
+}
+
+var startPoint = 0;
+let numbersOfElementsToShow = 4;
+
+function moreArticles(startPoint, numbersOfElementsToShow) {
+    for (i = startPoint; i < numbersOfElementsToShow; i++) {
+
+        if (numbersOfElementsToShow > data.length) {
+            numbersOfElementsToShow == data.length;
+        }
+        if (startPoint >= data.length) {
+            containerToCreat = document.createElement('');
+        }
+        var imgSrc = data[i].imgSrc;
+        var text = data[i].blogText;
+        var description = getCardsInfo(data[i])
+        var containerToCreat = document.createElement('article');
+        containerToCreat.innerHTML = `<img src="${imgSrc}" alt="Изображение" title="Изображение">
+        <p class="cardsBlogs"> Блоги </p>
+        <p class="cardsText"> ${text}
+           <div class="articleInfo">
+             <div class="articleViwes">
+             ${description}
+           </div>
+        </div>`;
+
+        var blogsList = document.getElementById('articles');
+        blogsList.appendChild(containerToCreat);
+        getCardsInfo(containerToCreat);
+    }
+}
+
+moreArticlesButton.addEventListener("click", (function() {
+     pastTemplate( )}))
+//startPoint = startPoint + 5, numbersOfElementsToShow += 5);
+
+
+for (i = 0; i < 4; i++) {
+    // pastTemplate();
+    // moreArticles();
+    // createCard(i);
+
+}
